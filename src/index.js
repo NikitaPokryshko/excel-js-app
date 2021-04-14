b5242bb8f1ab07ca23952f64d29cc9fc25715b21
Nikita Pokryshko <pokryshko.n@gmail.com>
2021-04-15 01:42:07 +0300

    [1] Add basic webpack config with main plugins and modules

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// !!! Important: Keep polyfills in the top of the file
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+import './module';
+
+import '@/scss/index.scss';
+
+console.log('starting point')
